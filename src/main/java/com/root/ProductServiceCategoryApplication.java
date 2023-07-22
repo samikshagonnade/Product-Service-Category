c4d1e5e471272ea4091faa9c31d2f1bcb0ec39a6
Samiksha <samigonnade@gmail.com>
2023-07-22 19:16:46 +0530

    first commit

---
+++ src/main/java/com/root/ProductServiceCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceCategoryApplication.class, args);
+	}
+
+}
